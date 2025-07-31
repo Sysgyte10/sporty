b0306fa89ed4cc5aeaec256ba2344f736bab4597
AdebayoBunmiSolomon <hardeybayorsamuel11@gmail.com>
2025-07-31 07:44:11 +0100

    added keysats, and other ui components

--- src/components/app/one-match/MatchTab.tsx
+++ src/components/app/one-match/MatchTab.tsx
@@ -2,7 +2,7 @@ import { CustomText } from "@src/components/shared";
 import { DVH, DVW, moderateScale } from "@src/resources/responsiveness";
 import { ScrollContainer } from "@src/screens/ScrollContainer";
 import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { Platform, StyleSheet, View } from "react-native";
 import { Image } from "expo-image";
 import Animated, { FadeIn } from "react-native-reanimated";
 import { TableTab, TeamStatsTab } from "../fixture-info";
@@ -11,6 +11,24 @@ import SwitchToggle from "react-native-switch-toggle";
 import { FontAwesome, MaterialCommunityIcons } from "@expo/vector-icons";
 import { colors } from "@src/resources/color/color";
 import { oneMatchInfo } from "@src/constants/onematch";
+
+const keyStats = [
+  {
+    title: "Possessions %",
+    clubName: "PSG",
+    stats: [75, 25],
+  },
+  {
+    title: "Shots on target",
+    clubName: "BOT",
+    stats: [2, 4],
+  },
+  {
+    title: "Corners",
+    clubName: "PSG",
+    stats: [10, 1],
+  },
+];
 
 export const MatchTab: React.FC<{}> = () => {
   const [isSwitchOn, setIsSwitchOn] = useState<boolean>(false);
@@ -23,7 +41,7 @@ export const MatchTab: React.FC<{}> = () => {
         <View style={styles.arrowContainer}>
           <MaterialCommunityIcons
             name='arrow-collapse'
-            size={moderateScale(10)}
+            size={moderateScale(15)}
             color={colors.white}
           />
         </View>
@@ -237,8 +255,179 @@ export const MatchTab: React.FC<{}> = () => {
       <TableTab goalScorerData={footballFixtures[0]?.matches[0]?.topScorers} />
 
       <View
+        style={[
+          styles.timeLineCard,
+          { flexDirection: "column", marginBottom: moderateScale(10) },
+        ]}>
+        <View style={styles.keyStatsIconContainer}>
+          <CustomText type='semi-bold' size={12} lightGrey>
+            Key stats
+          </CustomText>
+          <View style={styles.arrowContainer}>
+            <MaterialCommunityIcons
+              name='arrow-collapse'
+              size={moderateScale(15)}
+              color={colors.white}
+            />
+          </View>
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            width: "100%",
+          }}>
+          {keyStats &&
+            keyStats.map((item, index) => (
+              <View style={styles.keyStatsSubCard} key={index}>
+                <CustomText size={10} type='medium' lightGrey>
+                  {item.title}
+                </CustomText>
+                <CustomText size={12} type='semi-bold' lightGrey>
+                  {item.clubName}
+                </CustomText>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    flex: 1,
+                  }}>
+                  {item?.stats?.map((value, index) => (
+                    <View
+                      style={{ flex: 1, gap: moderateScale(5) }}
+                      key={index}>
+                      <CustomText type='semi-bold' size={12} lightGrey>
+                        {value}
+                      </CustomText>
+                      <View
+                        style={{
+                          width: value > 50 ? "90%" : "70%",
+                          height: DVH(0.5),
+                          borderRadius: moderateScale(5),
+                          backgroundColor:
+                            value > 50 ? colors.purple : colors.lightGrey,
+                        }}
+                      />
+                    </View>
+                  ))}
+                </View>
+              </View>
+            ))}
+        </View>
+      </View>
+
+      <View style={styles.timeLineCard}>
+        <CustomText type='medium' size={12} lightGrey>
+          Relevant Matches
+        </CustomText>
+      </View>
+
+      <CustomText
+        type='semi-bold'
+        size={12}
+        lightGrey
+        style={{ textAlign: "center", paddingVertical: moderateScale(10) }}>
+        JUNE 19 (FT)
+      </CustomText>
+
+      {Array.from({ length: 2 }).map((_, index) => (
+        <View style={styles.scoreContainerCard}>
+          <View style={styles.scoreContainer}>
+            <View style={styles.clubImgContainer}>
+              <Image
+                source={
+                  index === 0
+                    ? require("@src/assets/png/totheham.png")
+                    : require("@src/assets/png/liverpool.png")
+                }
+                contentFit='fill'
+                style={styles.clubImg}
+              />
+            </View>
+            <CustomText type='bold' size={40} white>
+              {index === 0 ? "1 - 0" : "0 - 1"}
+            </CustomText>
+            <View style={styles.clubImgContainer}>
+              <Image
+                source={
+                  index === 0
+                    ? require("@src/assets/png/chelsea.png")
+                    : require("@src/assets/png/arsenal.png")
+                }
+                contentFit='fill'
+                style={styles.clubImg}
+              />
+            </View>
+          </View>
+          <View
+            style={[
+              styles.scoreContainer,
+              {
+                paddingHorizontal: moderateScale(15),
+                paddingBottom: moderateScale(10),
+              },
+            ]}>
+            <CustomText type='semi-bold' size={10} lightGrey>
+              {index === 0 ? "TOTHEHAM" : "LIVERPOOL"}
+            </CustomText>
+            <CustomText type='semi-bold' size={10} lightGrey>
+              FINISHED
+            </CustomText>
+            <CustomText type='semi-bold' size={10} lightGrey>
+              {index === 0 ? "CHELSEA" : "ARSENAL"}
+            </CustomText>
+          </View>
+        </View>
+      ))}
+
+      <View
+        style={[
+          styles.timeLineCard,
+          {
+            flexDirection: "column",
+          },
+        ]}>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            width: "100%",
+            paddingBottom: moderateScale(5),
+          }}>
+          <CustomText type='semi-bold' size={12} lightGrey>
+            CONFIRMED LINEUPS
+          </CustomText>
+          <View style={styles.arrowContainer}>
+            <MaterialCommunityIcons
+              name='arrow-collapse'
+              size={moderateScale(15)}
+              color={colors.white}
+            />
+          </View>
+        </View>
+        <View
+          style={{
+            width: "100%",
+            height: DVH(40),
+            overflow: "hidden",
+          }}>
+          <Image
+            contentFit='fill'
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+            source={require("@src/assets/png/football-pitch.png")}
+          />
+        </View>
+      </View>
+
+      <View
         style={{
-          paddingVertical: DVH(20),
+          paddingVertical: DVH(10),
         }}
       />
     </ScrollContainer>
@@ -281,5 +470,43 @@ const styles = StyleSheet.create({
   recycleIcon: {
     width: DVW(4),
     height: DVH(2),
+  },
+  keyStatsIconContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+    paddingBottom: moderateScale(5),
+  },
+  keyStatsSubCard: {
+    paddingVertical: moderateScale(10),
+    paddingHorizontal: moderateScale(7),
+    borderRadius: moderateScale(7),
+    backgroundColor: colors.black,
+    gap: moderateScale(5),
+    overflow: "hidden",
+    width: "32%",
+  },
+
+  clubImgContainer: {
+    overflow: "hidden",
+    borderRadius: moderateScale(10),
+    padding: moderateScale(10),
+    backgroundColor: "#242222",
+  },
+  clubImg: {
+    width: DVW(12),
+    height: Platform.OS === "ios" ? DVH(5) : DVH(6),
+  },
+  scoreContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: moderateScale(10),
+  },
+  scoreContainerCard: {
+    backgroundColor: "#242222",
+    marginBottom: moderateScale(10),
+    borderRadius: moderateScale(10),
   },
 });
